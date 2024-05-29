bd9af7c93c7d1b12e8330484ac36c38ca13a5966
Ricardo Pedroso <rmdpedroso@gmail.com>
2024-05-29 23:08:10 +0100

    small changes

--- config.py
+++ config.py
@@ -1,24 +1,13 @@
 import os
 from origamibot import OrigamiBot as Bot
 
-# NODE_URL = 'http://localhost:9231/json_rpc'
+# NODE_URL = 'http://localhost:9231'
 NODE_URL = 'http://154.38.165.93:9231'
 
 TOKEN = os.getenv('TOKEN')  # bot token
 TO = os.getenv('TO')  # group chat id
 
 if not TOKEN or not TO:
-    raise ValueError('Need env vars TOKEN and TO')
-
-
-class FakeBot:
-    def __init__(self, *args):
-        pass
-
-    def send_message(self, chat_id, message, **kwargs):
-        print('_' * 60)
-        print(message)
-        print('_' * 60)
-
+    raise ValueError('TOKEN and TO must be configured')
 
 bot = Bot(TOKEN)
